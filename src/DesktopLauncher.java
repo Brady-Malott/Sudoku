d0ccc27b67cd62549262fa029f0ea5373e4ca774
Brady-Malott <55215446+Brady-Malott@users.noreply.github.com>
2020-05-10 19:43:19 -0400

    Pushed Desktop Launcher

---
+++ src/DesktopLauncher.java
@@ -0,0 +1,14 @@
+package com.sudoku.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.sudoku.game.Sudoku;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Sudoku(), config);
+		config.width = 1000;
+		config.height = 800;
+	}
+}
